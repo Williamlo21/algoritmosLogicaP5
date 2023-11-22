370289166101fa4a6e8bda26a9ae37dc8b7467a1
William lopez <lopezrebellonw@gmail.com>
2023-11-22 09:17:09 -0500

    creamos modelo para partidos

---
+++ models/Partidos.php
@@ -0,0 +1,61 @@
+<?php
+
+class Respuestas
+{
+    public $equipo;
+    public $ganados;
+    public $perdidos;
+    public $empatados;
+
+    public function __construct()
+    {
+    }
+    public function getEquipo()
+    {
+        return $this->equipo;
+    }
+    public function setEquipo($equipo)
+    {
+        $this->equipo = $equipo;
+
+        return $this;
+    }
+    public function getGanados()
+    {
+        return $this->ganados;
+    }
+    public function setGanados($ganados)
+    {
+        $this->ganados = $ganados;
+
+        return $this;
+    }
+    public function getPerdidos()
+    {
+        return $this->perdidos;
+    }
+    public function setPerdidos($perdidos)
+    {
+        $this->perdidos = $perdidos;
+
+        return $this;
+    }
+    public function getEmpatados()
+    {
+        return $this->empatados;
+    }
+    public function setEmpatados($empatados)
+    {
+        $this->empatados = $empatados;
+
+        return $this;
+    }
+    public function darResultado()
+    {
+        $pGanados = $this->ganados;
+        $pEmpatados = $this->empatados;
+        $pPerdidos = $this->perdidos;
+        $res = ($pGanados * 3) + ($pEmpatados * 1) + ($pPerdidos * 0);
+        return $res;
+    }
+}
